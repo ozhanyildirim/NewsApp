6ae297dd7b743c2099bd5759df5ad4c51c926610
ozhan <ozhannuri@gmail.com>
2020-12-13 18:09:14 +0300

    Kayıt ol ekranı tamamlandı

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text ,StyleSheet, TextInput,Platform ,TouchableOpacity,Dimensions, Image} from 'react-native';
+import { View, Text ,StyleSheet, StatusBar, TextInput,Platform ,TouchableOpacity,Dimensions, Image} from 'react-native';
 
 const SignUpScreen = ({navigation}) => {
 
@@ -9,39 +9,41 @@ const SignUpScreen = ({navigation}) => {
         <View style={styles.header}>
           <Text style={styles.text_header}>Kayıt Ol !</Text>
         </View >
+
+
         <View style={styles.footer}>
           <Text style={styles.text_footer}>Email</Text>
           <View style={styles.action}>
             <TextInput placeholder="Email adresiniz"
             style={styles.TextInput}
-            autoCapitalize="none"
-       
-            />
-      </View >
-              <Text style={[styles.text_footer ,{ marginTop : 35}]} >Şifre</Text>
-              <View style={styles.action}>
+            autoCapitalize="none" />
+          </View >
+          <Text style={[styles.text_footer ,{ marginTop : 35}]} >Şifre</Text>
+            <View style={styles.action}>
             <TextInput placeholder="Şifrenizi Girin"
             secureTextEntry={true}
             style={styles.TextInput}
             autoCapitalize="none"/>
      </View>
-     </View >
-              <Text style={[styles.text_footer ,{ marginTop : 35}]} >Tekrar Şifre</Text>
+     
+     <Text style={[styles.text_footer ,{ marginTop : 35}]} >Şifre Doğrula</Text>
               <View style={styles.action}>
             <TextInput placeholder="Şifrenizi Tekrar Girin"
             secureTextEntry={true}
             style={styles.TextInput}
             autoCapitalize="none"/>
-     </View>
-       <View  style={styles.opacity}> 
-        <TouchableOpacity > 
+     </View >
+     <View  style={styles.opacity}> 
+        <TouchableOpacity onPress={() => navigation.goBack()}  > 
          <Image 
-           source={require('/app/app/assets/giris.png')} 
+           source={require('/app/app/assets/kayit.png')} 
            />
            </TouchableOpacity>
        </View>
+     </View>
+      
        </View>
-       </View>
+      // </View>
  );
  };
 
@@ -75,10 +77,37 @@ const styles=StyleSheet.create({
         flex :2 ,
         backgroundColor : '#fff',
         borderTopLeftRadius : 30,
-  borderTopRightRadius:30,
-  paddingHorizontal:20,
-  paddingVertical:30
-    }
+    borderTopRightRadius:30,
+    paddingHorizontal:20,
+    paddingVertical:30
+    },
+    opacity : {
+      padding : 30,
+      justifyContent : 'center',
+      alignItems : 'center'
+    },
+    textInput :{
+      flex:1,
+      marginTop:Platform.OS === 'ios' ? 0 : -12,
+      color :'#05375a',
+    },
+    text_footer : {
+      color : '#05375a',
+      fontSize :18
+    },
+    text_header : {
+      color : '#fff',
+      fontWeight : 'bold',
+      fontSize : 30,
+    
+    },
+    action :{
+      flexDirection:'row',
+      marginTop:10,
+      borderBottomWidth:1,
+      borderBottomColor:'#f2f2f2',
+      paddingBottom:5,
+    },
 
 
 
